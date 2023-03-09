82127ef48b918975681f484a0dc6462800d5e9ae
Wolchear <103861345+Wolchear@users.noreply.github.com>
2023-03-10 01:34:35 +0200

    Update getReferences.sh

--- getReferences.sh
+++ getReferences.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
+
 #1) the reference genome in FASTA format (9mm)
-wget -P../../references https://hgdownload.soe.ucsc.edu/goldenPath/mm9/bigZips/mm9.fa.gz
+wget -P../../references  https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M9/GRCm38.p4.genome.fa.gz
 
 #2)eference transcriptome in FASTA format
-wget -P../../references https://hgdownload.soe.ucsc.edu/goldenPath/mm9/bigZips/refMrna.fa.gz
+wget -P../../references https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M9/gencode.vM9.transcripts.fa.gz 
 
 #3) GTF file
-wget -P../../references  https://hgdownload.soe.ucsc.edu/goldenPath/mm9/bigZips/genes/mm9.refGene.gtf.gz
-
+wget -P../../references  https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M9/gencode.vM9.chr_patch_hapl_scaff.basic.annotation.gtf.gz
 
 #4) raw FASTQ files
 wget -P ../../inputs ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR898/007/SRR8985047/SRR8985047_1.fastq.gz
