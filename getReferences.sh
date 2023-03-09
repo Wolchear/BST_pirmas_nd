c786915c9b786f8fc9423415f724c9bd8fca00ae
Wolchear <103861345+Wolchear@users.noreply.github.com>
2023-03-09 17:53:42 +0200

    Update getReferences.sh

--- getReferences.sh
+++ getReferences.sh
@@ -2,4 +2,5 @@
 
 
 #get the reference genome in FASTA format (9mm)
-wget https://www.ncbi.nlm.nih.gov/assembly/GCF_000001635.18/
+#wget https://www.ncbi.nlm.nih.gov/assembly/GCF_000001635.18/
+echo "It works"
